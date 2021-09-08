bce429b30861b69d87ce6a6fb6eb9508b6fde7c1
impatidar <pankaj_patidar@hotmail.com>
2021-09-08 20:58:42 +0530

    Clock rendering each second

--- src/App.js
+++ src/App.js
@@ -105,15 +105,47 @@ import React from "react";
 
 //Clock Rendering with state
 
+// class Clock extends React.Component{
+//     constructor(props) {
+//         super(props);
+//         this.state = {date: new Date()};
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <h1> Hello</h1>
+//                 <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//             </div>
+//         );
+//     }
+// }
+
+//Clock rendering each second
 class Clock extends React.Component{
     constructor(props) {
         super(props);
         this.state = {date: new Date()};
     }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+    tick(){
+        this.setState({
+            date: new Date()
+        })
+    }
     render() {
-        return (
+        return(
             <div>
-                <h1> Hello</h1>
+                <h1>Hello, world!</h1>
                 <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         );
