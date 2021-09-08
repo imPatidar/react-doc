a1d3a2d9ce779f96c3827f8d59cca1f18da0fac6
impatidar <pankaj_patidar@hotmail.com>
2021-09-08 20:29:00 +0530

    Composing Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+// import ReactDOM from "react-dom";
+
+// const name = 'Pankaj';
+// function formatUser(user){
+//   return user.firstName + ' ' + user.lastName;
+// }
+//
+// const user = {
+//   firstName: 'Pankaj',
+//   lastName: 'Patidar'
+// }
+// const element = <h1>{formatUser(user)}</h1>;
+
+// const element = React.createElement(
+//     'h1',
+//     'Hi',
+//     formatUser(user)
+// )
+
+// function tick(){
+//     const element = (
+//         <div>
+//             <h1>Hello World!</h1>
+//             <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//         </div>
+//     )
+//     ReactDOM.render(element, document.getElementById('root'));
+// }
+// setInterval(tick, 1000);
+
+
+//Functional Component
+// function Welcome(props) {
+//     return <h1>Hello, {props.name}</h1>;
+// }
+//
+// const element = <Welcome name="Pankaj" />;
+
+
+//Class component
+
+// Composing components
+class Welcome extends React.Component{
+    render() {
+        return<h1>Hello, {this.props.name}</h1>;
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div>
+            <Welcome name="Pankaj"/>
+            <Welcome name="Avinash"/>
+        </div>
+    );
 }
 
 export default App;
