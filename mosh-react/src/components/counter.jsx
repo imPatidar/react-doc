8ac1f5a9f5994327d71b5f03ffbd36d661d7b27e
impatidar <pankaj_patidar@hotmail.com>
2021-09-15 08:42:57 +0530

    Passing data to components. i.e. Setting the default values with help of props.

--- mosh-react/src/components/counter.jsx
+++ mosh-react/src/components/counter.jsx
@@ -2,38 +2,39 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        value: this.props.value
     };
 
     handleIncrement = () => {
         // console.log(this.state.count);
-        this.setState({ count: this.state.count + 1 })
+        this.setState({value: this.state.value + 1})
     };
 
 
-    renderTags(){
-        if(this.state.tags.length === 0) return <p>"There are no tags!</p>;
-
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-    }
+    // renderTags(){
+    //     if(this.state.value === 0) return <p>"There are no tags!</p>;
+    //
+    //     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    // }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
+
     formatCount() {
-        const {count} = this.state;
-        return count === 0 ? "Zero" : count
+        const {value} = this.state;
+        return value === 0 ? "Zero" : value
     }
 
     render() {
+        // console.log("props", this.props);
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
-                {this.state.tags.length===0 && "There are no tags!"}
+                {this.state.count === 0 && "There are no tags!"}
             </div>
         );
     }
--- mosh-react/src/components/counters.jsx
+++ mosh-react/src/components/counters.jsx
@@ -4,16 +4,16 @@ import Counter from "./counter";
 class Counters extends Component {
     state={
         counters:[
-            {id: 1, key: 0},
+            {id: 1, key: 4},
             {id: 2, key: 0},
-            {id: 3, key: 0},
+            {id: 3, key: 2},
             {id: 4, key: 0},
         ]
     }
     render() {
         return (
             <div>
-                {this.state.counters.map(counter => <Counter key={counter.id} />)}
+                {this.state.counters.map(counter => <Counter key={counter.id} value={counter.key} selected/>)}
             </div>
         );
     }
