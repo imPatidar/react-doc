428f416ea6a3e4758c61605b98c80777dffd4515
impatidar <pankaj_patidar@hotmail.com>
2021-09-17 08:49:26 +0530

    Added the delete functionality.

--- mosh-react/src/components/counter.jsx
+++ mosh-react/src/components/counter.jsx
@@ -2,11 +2,11 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-        value: this.props.value
+        value: this.props.counter.key
     };
 
     handleIncrement = () => {
-        // console.log(this.state.count);
+        // console.log(this.state.value);
         this.setState({value: this.state.value + 1})
     };
 
@@ -32,9 +32,9 @@ class Counter extends Component {
         // console.log("props", this.props);
         return (
             <div>
-                <h4>Title #{this.props.id}</h4>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
                 {this.state.count === 0 && "There are no tags!"}
             </div>
         );
--- mosh-react/src/components/counters.jsx
+++ mosh-react/src/components/counters.jsx
@@ -11,13 +11,22 @@ class Counters extends Component {
         ]
     }
 
+    handleDelete = (counterId) => {
+        // console.log("Event handler called", counterId);
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters});
+    }
+
     render() {
         return (
             <div>
                 {this.state.counters.map(counter =>
-                    <Counter key={counter.id} value={counter.key} id={counter.id}>
-                    </Counter>
-                    )}
+                    <Counter
+                        key={counter.id}
+                        onDelete={this.handleDelete}
+                        counter = {counter}
+                    />
+                )}
             </div>
         );
     }
